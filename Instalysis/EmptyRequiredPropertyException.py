3c4fbb20a1e596a6dc47a87393dba504a10b4cf3
Mr. DSP <ahamdsp@protonmail.com>
2022-05-08 20:57:20 +0530

    InstgramAccount class body added.

---
+++ Instalysis/EmptyRequiredPropertyException.py
@@ -0,0 +1,9 @@
+"""
+author: DSP
+"""
+
+
+class EmptyRequiredPropertyException(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
--- Instalysis/InstagramAccount.py
+++ Instalysis/InstagramAccount.py
@@ -2,25 +2,33 @@
 author: DSP
 """
 
+
 class InstagramAccount:
+    # -*- Private Members -*-
+
     __username: str
     __followers: set
     __followings: set
     __unfollowers: set
     __unfollowees: set
     __name: str
+    __hd_profile_picture_link: str
+    __instagram_profile_link: str
+
+    # -*- Constructor -*-
+
     def __init__(self):
         self.__username = None
         self.__name = None
+        self.__hd_profile_picture_link = None
+        self.__instagram_profile_link = None
         self.__followers = set()
         self.__unfollowers = set()
         self.__followings = set()
         self.__unfollowees = set()
-        
-    def __init__(self, instagram_username: str) -> None:
-        self.__init__()
-        self.__username = instagram_username
-    
+
+    # -*- Setter Methods -*-
+
     def set_username(self, instagram_username: str) -> None:
         self.__username = instagram_username
 
@@ -30,14 +38,25 @@ class InstagramAccount:
     def set_followings(self, followings: set) -> None:
         self.__followings = followings
 
-    def set_unfollowers(self, follower: set) -> None:
+    def set_unfollowers(self) -> None:
         self.__unfollowers = self.__followings.difference(self.__followers)
 
-    def set_unfollowings(self, followings: set) -> None:
+    def set_unfollowings(self) -> None:
         self.__unfollowees = self.__followers.difference(self.__followings)
 
     def set_name(self, name: str) -> None:
         self.__name = name
+
+    def set_instagram_profile_link(self, link: str) -> None:
+        self.__instagram_profile_link = link
+
+    def set_hd_profile_picture_link(self, link: str) -> None:
+        self.__hd_profile_picture_link = link
+
+    # -*- Getter Methods -*-
+
+    def get_username(self) -> str:
+        return self.__username
 
     def get_name(self) -> str:
         return self.__name
@@ -66,3 +85,8 @@ class InstagramAccount:
     def get_unfollowees_count(self) -> int:
         return len(self.__unfollowees)
 
+    def get_instagram_profile_link(self) -> str:
+        return self.__instagram_profile_link
+
+    def get_hd_profile_picture_link(self) -> str:
+        return self.__hd_profile_picture_link
